69f9850b6a48e66c0b128fc8deda8653d950da99
GirinathRG <153888500+GirinathRG@users.noreply.github.com>
2023-12-18 15:35:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,24 @@ import numpy as np
 import cv2
 import os
 
-# Load your trained model
+# Construct the absolute path to the model file
 model_path = r"C:\Users\Unknown\Desktop\Aadhaar Detection\models\aadhaar.h5"
+
+# Print the path for debugging purposes
+print("Model Path:", model_path)
+
+# Load your trained model
 model = load_model(model_path)
 
-def predict_aadhaar(img_array):
-    # Normalize the image
-    img_array /= 255.0
+def predict_aadhaar(image_path):
+    # Load and preprocess the image
+    img = image.load_img(image_path, target_size=(128, 128))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0  # Normalize the image
 
     # Make predictions
-    prediction = model.predict(np.expand_dims(img_array, axis=0))
+    prediction = model.predict(img_array)
 
     # Assuming your model has two classes (0: Not Aadhaar, 1: Aadhaar)
     predicted_class = np.argmax(prediction)
@@ -28,15 +36,15 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Load and preprocess the uploaded image
-        img = image.load_img(uploaded_file, target_size=(128, 128))
-        img_array = image.img_to_array(img)
+        image_path = "temp_image.jpg"
+        with open(image_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
 
         # Display the uploaded image
-        st.image(img, caption="Uploaded Image", use_column_width=True)
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
         # Make prediction
-        if predict_aadhaar(img_array):
+        if predict_aadhaar(image_path):
             st.success("This is an Aadhaar card!")
         else:
             st.warning("This is not an Aadhaar card.")
